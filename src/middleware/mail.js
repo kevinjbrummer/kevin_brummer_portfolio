198327e475e7b5018b03b690faad4a0f74ce509c
Kevin Brummer <kjbrummernsf@gmail.com>
2023-05-21 21:51:20 +0900

    add user contact mail

--- src/middleware/mail.js
+++ src/middleware/mail.js
@@ -1,4 +1,5 @@
 const mailer = require('nodemailer');
+const htmlToText = require('nodemailer-html-to-text').htmlToText;
 
 const mailOptions = {
   service: 'Gmail',
@@ -13,20 +14,62 @@ const mailOptions = {
       rejectUnauthorized: false
     }
   })
+}
 
+const htmlToTextOptions = {
+  wordwrap: 130,
 }
 
 const transporter = mailer.createTransport(mailOptions);
+transporter.use('compile', htmlToText(htmlToTextOptions));
+
+function getBodyHtml(req) {
+  return `<p>
+  Hello ${req.body.name}!
+</p>
+<p>
+Thank you for taking the time to send a message to me.
+<br>
+I'm usually pretty busy, but I'll try to get back to you as soon as I can.
+</p>
+<p>Below, you can review the contents of your message</p>
+<p>
+  ----------------------------
+  <br>
+  Name: ${req.body.name}
+  <br>
+  Email: ${req.body.email}
+  <br>
+  Message: 
+  <br>
+  ${req.body.message}
+  <br>
+  ----------------------------
+</p>
+<p>
+  Thanks again, and have a wonderful day!
+</p>
+<p>
+  Sincerely,
+  Kevin Brummer
+</p>
+`;
+}
 
 function sendMail(req, res, next) {
   transporter.sendMail({
-    from: process.env.EMAIL_ADDRESS,
+    from: `Kevin Brummer <${process.env.EMAIL_ADDRESS}>`,
     to: req.body.email,
-    subject: "Mail Test",
-    text: "Mail Body"
+    subject: "Contact Request Sent - kevinbrummer.com",
+    html: getBodyHtml(req)
   }, function (error, info) {
     if (error) {
-      console.log(error)
+      console.log(error);
+      return res.render('index', {
+        title: 'Home',
+        recaptcha_site_key: process.env.RECAPTCHA_SITE_KEY,
+        system_error: true
+      });
     }
     next();
   });
