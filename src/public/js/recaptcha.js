756e25dcdd29fa8f7b4b4f8652421da1ed8c523d
Kevin Brummer <kjbrummernsf@gmail.com>
2023-05-27 16:21:58 +0900

    fix multiple submits

--- src/public/js/recaptcha.js
+++ src/public/js/recaptcha.js
@@ -1,21 +1,22 @@
 const submitButton = document.getElementById('submitBtn');
 
-    submitButton.addEventListener('click', submitContactForm);
+  submitButton.addEventListener('click', submitContactForm);
 
-    function submitContactForm(e) {
-      e.preventDefault();
-      grecaptcha.ready(function() {
-        grecaptcha.execute(recaptchaSiteKey, {action: 'submit'}).then(function(token) {
-            const contactForm = document.getElementById('contact-form');
-            const recaptchaElement = document.createElement('input');
+  function submitContactForm(e) {
+    e.preventDefault();
+    submitButton.setAttribute('disabled', true);
+    grecaptcha.ready(function() {
+      grecaptcha.execute(recaptchaSiteKey, {action: 'submit'}).then(function(token) {
+          const contactForm = document.getElementById('contact-form');
+          const recaptchaElement = document.createElement('input');
 
-            recaptchaElement.setAttribute('type', 'hidden');
-            recaptchaElement.setAttribute('value', token);
-            recaptchaElement.setAttribute('name', 'grecaptcha');
+          recaptchaElement.setAttribute('type', 'hidden');
+          recaptchaElement.setAttribute('value', token);
+          recaptchaElement.setAttribute('name', 'grecaptcha');
 
-            contactForm.appendChild(recaptchaElement);
+          contactForm.appendChild(recaptchaElement);
 
-            contactForm.submit();
-        });
+          contactForm.submit();
       });
-    }
+    });
+  }
