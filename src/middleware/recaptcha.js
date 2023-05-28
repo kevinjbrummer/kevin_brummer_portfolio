ae772e078ac2ad9f773e646df22bd007d109fded
Kevin Brummer <kjbrummernsf@gmail.com>
2023-05-29 08:05:26 +0900

    add fetch

--- src/middleware/recaptcha.js
+++ src/middleware/recaptcha.js
@@ -1,4 +1,5 @@
-import fetch from "node-fetch";
+const fetch = require("node-fetch");
+
 
 async function tokenVerify(token) {
   const res = await fetch('https://www.google.com/recaptcha/api/siteverify', {
