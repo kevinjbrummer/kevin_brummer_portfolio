ef75f5dedadc7e19651eb58c3b1893e656576629
Kevin Brummer <k-brummer@ankh-systems.co.jp>
2023-11-20 16:50:53 +0900

    404ページを実装

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,3 +1,27 @@
+import Footer from "@/components/Footer";
+import Link from "next/link";
+
 export default function NotFound() {
-  return <div>Custom 404 Page</div>;
+  return (
+    <>
+      <header className="header">
+        <nav className="header__links">
+          <Link tabIndex={0} href="/">
+            Kevin Brummer <br /> Software Engineer
+          </Link>
+        </nav>
+      </header>
+      <main className="error">
+        <div>
+          <h1>
+            404 <br /> Page Not Found
+          </h1>
+          <Link className="home-link" href={"/"}>
+            Home
+          </Link>
+        </div>
+      </main>
+      <Footer />
+    </>
+  );
 }
