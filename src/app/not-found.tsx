80293a965795781951d1e0411c106eacb12e6257
Kevin Brummer <k-brummer@ankh-systems.co.jp>
2023-11-21 17:39:34 +0900

    prodのための修正

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,5 +1,44 @@
 import Footer from "@/components/Footer";
 import Link from "next/link";
+import "./globals.css";
+import "./reset.css"
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Kevin Brummer | Software Engineer",
+  description: "Portfolio Website for Kevin Brummer.",
+  viewport: "width=device-width, initial-scale=1.0",
+  keywords: [
+    "Kevin Brummer",
+    "Software",
+    "Engineer",
+    "Japan",
+    "Osaka",
+    "UMN",
+    "University of Minnesota",
+    "Computer Science",
+    "Portfolio",
+    "Cherry Blossoms",
+    "AWS",
+    "React",
+    "Next",
+    "Ruby",
+    "Rails",
+  ],
+  openGraph: {
+    type: "website",
+    url: "https://www.kevinbrummer.com",
+    title: "Kevin Brummer | Software Engineer",
+    description: "Portfolio Website for Kevin Brummer.",
+    siteName: "Kevin Brummer Portfolio",
+    images: [
+      {
+        url: "https://www.kevinbrummer.com/kevin_brummer_portfolio_og.jpg",
+      },
+    ],
+  },
+  authors: [{ name: "Kevin Brummer" }],
+};
 
 export default function NotFound() {
   return (
